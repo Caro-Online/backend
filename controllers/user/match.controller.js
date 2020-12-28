7b3ca874a6a69ebab544dc6561def8ae5df08c01
viethoang093 <viethoang.hcmus@gmail.com>
2020-12-28 12:37:30 +0700

    get curr match, set playing for player

--- controllers/user/match.controller.js
+++ controllers/user/match.controller.js
@@ -11,12 +11,12 @@ const createMatch = catchAsync(async (req, res) => {
 const getCurrentMatchByIdOfRoom = catchAsync(async (req, res) => {
     const { roomId } = req.params;
     const match = await matchService.getCurrentMatchByIdOfRoom(roomId);
-    res.status(httpStatus.OK).json({ success: true, match });
+    res.status(httpStatus.OK).json({ success: true, match: match[0] });
 })
 
 const addMove = catchAsync(async (req, res) => {
-    const { index, matchId } = req.body;
-    const match = await matchService.addMove(matchId, index);
+    const { index, matchId, xIsNext } = req.body;
+    const match = await matchService.addMove(matchId, index, xIsNext);
     res.status(httpStatus.OK).json({ success: true, match });
 })
 
--- services/match.service.js
+++ services/match.service.js
@@ -26,18 +26,20 @@ const createMatch = (players, roomId) => {
 // Id of room = _id (khác với RoomId)
 //Trả về match tạo sau nhất theo createdAt
 const getCurrentMatchByIdOfRoom = (roomId) => {
+    console.log("roomId: " + roomId)
     const match = Match.find({ room: roomId }).sort({ createdAt: -1 }).limit(1)
-    if (match) return match[0]
+    if (match) return match
     return null
 }
 
 //thêm 1 bước đi vào lich sử
-const addMove = (matchId, index) => {
+const addMove = (matchId, index, xIsNext) => {
     const filter = { _id: matchId };
     const update = {
         $push: {
             history: index,
         },
+        xIsNext: xIsNext
     };
     return Match.findOneAndUpdate(filter, update, { new: true })
 }
